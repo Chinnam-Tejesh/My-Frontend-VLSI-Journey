5a42eb1d7fbf75fa144399f8cbc6440a1925bb2d
tejesh-chinnam <tejesh.chinnam@gmail.com>
2026-01-27 16:12:06 +0530

    multiplexer code changes, updated to unpacked.

--- Projects/Multiplexer/dscr/multiplexer.sv
+++ Projects/Multiplexer/dscr/multiplexer.sv
@@ -7,6 +7,21 @@
 *
 *
 */
+module multiplexer #(parameter int in_bitwidth = 1, parameter int in_inputs = 16, parameter int log2ofin = ($clog2(in_inputs) <= 0) ? 1 : $clog2(in_inputs)) 
+                    (input logic [in_bitwidth - 1 : 0] in [ in_inputs - 1 : 0], input logic [log2ofin - 1 : 0] sel, output reg [in_bitwidth - 1 : 0] out);
+    
+    always @(*)begin
+        out = in[sel];
+        //multiply the sel with in_bitwidth to get starting index
+        //from the starting index go to MSB, in_bitwidth positions
+    end
+    //there is no logic that sends previous state's or specific state's output to the out. (no memory element needed to implement the expression)
+    //so i choosed always @(*) not always_comb
+endmodule
+
+
+
+/*      all input "in" bits are packed
 
 module multiplexer #(parameter int in_bitwidth = 1, parameter int in_inputs = 16, parameter int log2ofin = ($clog2(in_inputs) <= 0) ? 1 : $clog2(in_inputs)) 
                     (input logic [(in_bitwidth * in_inputs) - 1 : 0] in, input logic [log2ofin - 1 : 0] sel, output reg [in_bitwidth - 1 : 0] out);
@@ -19,3 +34,4 @@ module multiplexer #(parameter int in_bitwidth = 1, parameter int in_inputs = 16
     //there is no logic that sends previous state's or specific state's output to the out. (no memory element needed to implement the expression)
     //so i choosed always @(*) not always_comb
 endmodule
+*/
