151707ec01d7b5cc9f00408e9bd10be88c8eda48
tejesh-chinnam <tejesh.chinnam@gmail.com>
2026-01-28 20:47:54 +0530

    nothing

--- Projects/Demultiplexer/dsrc/demultiplexer.sv
+++ Projects/Demultiplexer/dsrc/demultiplexer.sv
@@ -7,21 +7,23 @@
 *
 *
 */
-module demultiplexer #(parameter int out_bitwidth = 1, 
-                       parameter int out_outputs = 16, 
-                       parameter int log2ofout = ($clog2(out_outputs > 0) ? $clog2(out_outputs): 1 )
-                       )(
-                       input logic [out_bitwidth - 1 : 0] in, 
-                       input logic [log2ofout - 1 : 0] sel, 
-                       output logic [out_bitwidth - 1 : 0] outs [out_outputs - 1 : 0]);
-    
-    always_comb begin
-        
-        outs =  '{default: '0}; // aleternative to 'b0, but for unpacked identifiers.
-        
-        if (sel < out_outputs) outs[sel] = in;
-        else out = '{default: '0}; // not needed, just being safe.
+module demultiplexer #(
+    parameter int out_bitwidth = 1,
+    parameter int out_outputs = 16,
+    parameter int log2ofout = ($clog2(out_outputs > 0) ? $clog2(out_outputs) : 1)
+) (
+    input logic [out_bitwidth - 1 : 0] in,
+    input logic [log2ofout - 1 : 0] sel,
+    output logic [out_bitwidth - 1 : 0] outs[out_outputs - 1 : 0]
+);
 
-    end
+  always_comb begin
+
+    outs = '{default: '0};  // aleternative to 'b0, but for unpacked identifiers.
+
+    if (sel < out_outputs) outs[sel] = in;
+    else out = '{default: '0};  // not needed, just being safe.
+
+  end
 
 endmodule
